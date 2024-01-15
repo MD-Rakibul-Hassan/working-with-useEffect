53939f8feb37378566c35b2e925f239e8ac2a91d
Rakibul-Hassan-Shehab <mdrakibulhassan2425@gmail.com>
2024-01-15 08:27:34 +0600

    Handle error and handle loging data in fetch

--- src/Components/DataFetchWithUseEffect/Fetch_Data.jsx
+++ src/Components/DataFetchWithUseEffect/Fetch_Data.jsx
@@ -1,17 +1,36 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
+const lodingMessage = "Please waite some moment"
 const Fetch_Data = () => {
     const [photo,setPhoto] = useState(null);
+    const [loding,setLoding] = useState(true);
+    const [error,setError] = useState(null);
     useEffect( () => {
         fetch ("https://jsonplaceholder.typicode.com/users")
         .then( (response) => {
+            if(!response.ok) {
+                throw Error ("Fetch is not done !")
+            }
             return response.json()
         })
-        .then((data) => setPhoto(data))
+        .then((data) => {
+            setPhoto(data);
+            setLoding(false)
+        })
+        .catch( (error) => {
+            setError(error.message);
+            setLoding(false)
+        })
     },[])
     return (
         <div>
+            {
+                error && <p>{error}</p>
+            }
+
+            {
+                loding && lodingMessage
+            }
             {
                 photo &&  photo.map( (itme) => {
                     return <h1 key={itme.id}>{itme.name}</h1>
